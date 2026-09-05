8fa3cc720f399ab340180178a2b678b9daa0c5fc
Jatin Sharma <jatinw3wc@gmail.com>
2026-09-05 18:10:12 +0530

    data types

---
+++ lesson-datatypes.js
@@ -0,0 +1,38 @@
+console.log( 3 + 
+    3);  // dont write code like this  code reading is difficult and it is not a good practice.
+    //  Always write code in a single line or use proper indentation.
+
+    console.log(3 + 3); // This is a better way to write the code. It is more
+    //  readable and easier to understand.
+    
+    let name = "Jatin"; // this is string data type. It is used to store text.
+    name = "Rahul"; // we can change the value of a variable. Here we are changing the 
+    // value of name from "Jatin" to "Rahul".
+    let age = 25; // this is number data type. It is used to store numbers.
+    let isStudent = true; // this is boolean data type. It is used to store true or false values.
+
+    let number = 38947649269462n; // this is BigInt data type. It is used to store large numbers.
+
+    //null is a special data type that represents the absence of a value.
+    //  It is used to indicate that a variable has no value.
+    let emptyValue = null; // here we are assigning null to the variable emptyValue.
+
+
+    let product; // undefined
+
+    // symbols is used to unique
+    // object
+    
+    console.log(typeof "name"); // typeof operator is used to check the data type of a variable. 
+    // It returns a string that represents the data type of the variable.
+
+
+    // In JavaScript, there are 7 data types. They are:
+    // 1. String
+    // 2. Number
+    // 3. Boolean
+    // 4. BigInt
+    // 5. Null
+    // 6. Undefined
+    // 7. Symbol
+    
