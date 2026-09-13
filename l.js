3422d6e6453adf1603625d3a5f09db623b1428ff
Jatin Sharma <jatinw3wc@gmail.com>
2026-09-13 18:02:33 +0530

    function-changes

--- l.js
+++ l.js
@@ -1,26 +1,2 @@
-let user1 = {
-    name: "jatin",
-    id: 200,
-    job: "frontend-devloper",
-}
-let user2 = {
-    name: "mainsh",
-    id: 202,
-    job: "tester",
-}
-let user3 = {
-    name: "maddy",
-    id: 203,
-    job: "editor",
-}
-let user4 = {
-    name: "manik",
-    id: 204,
-    job: "editor",
-}
-let user5 = {
-    name: "manik sharma",
-    id: 205,
-    job: "editor",
-}
-
+console.log(name);
+const name = john
---
+++ new.ts
@@ -0,0 +1,3 @@
+let username = "jatin";
+let id = 201;
+console.log(username , id);
